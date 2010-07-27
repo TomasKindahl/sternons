bc397991c1a7172098750746948418135e78bc82
tomki923 <tomki923@d0dc9f33-f6c6-3cc5-8ec7-4276afe3e213>
2010-07-27 11:37:17 +0000

    subst tabs for spaces in all src

--- trunk/mkmap/uctype.c
+++ trunk/mkmap/uctype.c
@@ -3,20 +3,20 @@
 
 /* uchar is White Space? */
 int isuws(uchar uch) {
-	return (uch == L' ') || (uch == L'\t') || (uch == L'\n');
+    return (uch == L' ') || (uch == L'\t') || (uch == L'\n');
 }
 
-	/* uchar is alphabetic char? (only Latin considered yet) */
+    /* uchar is alphabetic char? (only Latin considered yet) */
 int isualpha(uchar uch) {
-	if (L'A' <= uch && uch <= L'Z') return 1;
-	if (L'a' <= uch && uch <= L'z') return 1;
-	if (L'À' <= uch && uch <= L'ʯ' && uch != L'×' && uch != L'÷') return 1;
-	return 0;
+    if (L'A' <= uch && uch <= L'Z') return 1;
+    if (L'a' <= uch && uch <= L'z') return 1;
+    if (L'À' <= uch && uch <= L'ʯ' && uch != L'×' && uch != L'÷') return 1;
+    return 0;
 }
-	/* uchar is digit? (only Latin considered yet) */
+    /* uchar is digit? (only Latin considered yet) */
 int isunum(uchar uch) {
-	if (L'0' <= uch && uch <= L'9') return 1;
-	return 0;
+    if (L'0' <= uch && uch <= L'9') return 1;
+    return 0;
 }
 
 /* uchar string functions (cygwin has a UTF-16 wchar_t, we assume UTF-32 - int) */
