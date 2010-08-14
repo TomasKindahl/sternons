82b3c1f3c20d34e049a10cd2b03ca8880ce14b0b
tomki923 <tomki923@d0dc9f33-f6c6-3cc5-8ec7-4276afe3e213>
2010-08-14 14:43:42 +0000

    fully added LGPL (ascertained compilability from pre ver)

--- mkmap/defs.h
+++ mkmap/defs.h
@@ -1,3 +1,21 @@
+/************************************************************************
+ *  This file is part of mkmap                                          *
+ *                                                                      *
+ *  mkmap is free software: you can redistribute it and/or modify it    *
+ *  under the terms of the GNU Lesser General Public License as         *
+ *  published by the Free Software Foundation, either version 3 of the  *
+ *  License, or (at your option) any later version.                     *
+ *                                                                      *
+ *  mkmap is distributed in the hope that it will be useful,            *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of      *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the       *
+ *  GNU Lesser General Public License for more details.                 *
+ *                                                                      *
+ *  You should have received a copy of the GNU Lesser General Public    *
+ *  License along with mkmap. If not, see                               *
+ *  <http://www.gnu.org/licenses/>.                                     *
+ ************************************************************************/
+
 #ifndef _DEFS_H
 #define _DEFS_H
 
