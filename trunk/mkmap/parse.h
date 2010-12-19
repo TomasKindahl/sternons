6a328e5c3038103650f910c01e9c98e26d4ddc03
tomki923 <tomki923@d0dc9f33-f6c6-3cc5-8ec7-4276afe3e213>
2010-12-19 16:46:57 +0000

    Index: parse.c 	added map_layer struct type to store layer declaration statement of program 	added parse function and constructor for layer declaration 	added program dump function accordingly Index: parse.h 	added map_layer struct type to store layer declaration statement of program Index: Makefile 	fixed bad diff index generation rule

--- trunk/mkmap/parse.h
+++ trunk/mkmap/parse.h
@@ -4,8 +4,14 @@
 #define NO_DEBUG 0
 #define DEBUG 1
 
+typedef struct _map_layer_S {
+    uchar *name;
+    struct _map_layer_S *next;
+} map_layer;
+
 typedef struct _procedure_S {
     uchar *name;
+    map_layer *layers;
     struct _procedure_S *next;
 } procedure;
 
@@ -20,5 +26,4 @@ int parse_program(char *program, program_state *pstat);
 void dump_program(program_state *pstat);
 
 #endif /* _PARSE_H */
-
 
