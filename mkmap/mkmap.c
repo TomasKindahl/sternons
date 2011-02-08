17d6dc779870f530ad800cc9adfe8b5e6fa914dd
tomki923 <tomki923@d0dc9f33-f6c6-3cc5-8ec7-4276afe3e213>
2011-02-08 15:23:28 +0000

    Rippout of structs star and star_view
    Index: Makefile
        added star.[ch], added rule img for image generation in Lunix
    Index: mkmap.c
        ripped out structs star and star_view, and their “methods” ...
    Index: star.c star.h
        ... into here

--- mkmap/mkmap.c
+++ mkmap/mkmap.c
@@ -22,10 +22,13 @@
 #include <string.h>
 #include <math.h>
 #include <time.h>        /* For time stamping the images in debug mode */
+/* General libraries */
 #include "usio.h"
 #include "ucstr.h"
 #include "token.h"
-/*#include "parse.h"*/
+/* Application libraries */
+/* TD:  #include "parse.h"*/
+#include "star.h"		/* star struct and DB views */
 
 /* ==================================================================== |
      Known bugs and features:                                           |
@@ -125,79 +128,6 @@ image_struct *new_image(uchar *name, int width, int height, double scale) {
 image_struct *image_set_projection(image_struct *image, lambert_proj *proj) {
     image->proj = proj;
     return image;
-}
-
-typedef struct _star_T {
-    /* TBA: int type; -- class */
-    double RA, DE, vmag;
-    struct _star_T *prev;
-    int HIP;
-} star;
-
-star *new_star(star *prev, int HIP, double RA, double DE, double vmag) {
-    star *res = (star *)malloc(sizeof(star));
-    res->RA = RA; res->DE = DE; res->vmag = vmag;
-    res->prev = prev;
-    res->HIP = HIP;
-    return res;
-}
-
-int star_cmp_by_mag(const void *P1, const void *P2) {
-	star *S1 = *((star **)P1);
-	star *S2 = *((star **)P2);
-	return (S1->vmag > S2->vmag) - (S1->vmag < S2->vmag);
-}
-
-int star_cmp_by_HIP(const void *P1, const void *P2) {
-	star *S1 = *((star **)P1);
-	star *S2 = *((star **)P2);
-	return (S1->HIP > S2->HIP) - (S1->HIP < S2->HIP);
-}
-
-typedef struct _star_view_S {
-    int size;
-    int next;
-    star **S;
-} star_view;
-
-star_view *new_star_view(int size) {
-    star_view *res = (star_view *)malloc(sizeof(star_view));
-    res->size = size;
-    res->next = 0;
-    res->S = (star **)malloc(sizeof(star *)*size);
-    return res;
-}
-
-int append_star(star_view *SV, star *S) {
-    if (SV->next == SV->size) {
-        int newsize = SV->size<<1; /* double it */
-        int ix;
-        star **NS = (star **)malloc(sizeof(star *)*newsize);
-        for(ix = 0; ix < SV->size; ix++) {
-            NS[ix] = SV->S[ix];
-        }
-        free(SV->S);
-        SV->S = NS;
-        SV->size = newsize;
-        SV->S[SV->next++] = S;
-        return 1;
-    }
-    SV->S[SV->next++] = S;
-    return 0;
-}
-
-void dump_stars(FILE *stream, star *S) {
-    fprintf(stream, "  (star: HIP =% 7i, α = %12.8f, δ = %12.8f, m = %4.2f)\n",
-            S->HIP, S->RA, S->DE, S->vmag);
-}
-
-void dump_stars_view(FILE *stream, star_view *SV) {
-    int ix;
-    fprintf(stream, "stars_by_mag: size=%i, alloc=%i {\n", SV->next, SV->size);
-    for(ix = 0; ix < SV->next; ix++) {
-        dump_stars(stream, SV->S[ix]);
-    }
-    fprintf(stream, "}\n");
 }
 
 typedef struct _program_state_S {
---
+++ mkmap/star.h
@@ -0,0 +1,45 @@
+/************************************************************************
+ *  Copyright (C) 2010 Tomas Kindahl                                    *
+ ************************************************************************
+ *  This file is part of mkmap                                          *
+ *                                                                      *
+ *  mkmap is free software: you can redistribute it and/or modify it    *
+ *  under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation, either version 3 of the License, or   *
+ *  (at your option) any later version.                                 *
+ *                                                                      *
+ *  mkmap is distributed in the hope that it will be useful,            *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of      *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the       *
+ *  GNU General Public License for more details.                        *
+ *                                                                      *
+ *  You should have received a copy of the GNU General Public License   *
+ *  along with mkmap. If not, see <http://www.gnu.org/licenses/>.       *
+ ************************************************************************/
+
+#ifndef _STAR_H
+#define _STAR_H
+
+typedef struct _star_T {
+    /* TBA: int type; -- class */
+    double RA, DE, vmag;
+    struct _star_T *prev;
+    int HIP;
+} star;
+
+typedef struct _star_view_S {
+    int size;
+    int next;
+    star **S;
+} star_view;
+
+star *new_star(star *prev, int HIP, double RA, double DE, double vmag);
+star_view *new_star_view(int size);
+int append_star(star_view *SV, star *S);
+int star_cmp_by_mag(const void *P1, const void *P2);
+int star_cmp_by_HIP(const void *P1, const void *P2);
+void dump_stars(FILE *stream, star *S);
+void dump_stars_view(FILE *stream, star_view *SV);
+
+#endif /* _STAR_H */
+
